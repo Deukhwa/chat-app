a0b9ba8b79da848fa0c9abcedc3abdee5d924ccd
Deukhwa <deukhwajeong@gmail.com>
2021-10-28 20:49:51 -0400

    Adding Socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
-  console.log('server on the port 3000')
+let count = 0
+
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection')
+
+  socket.emit('countUpdated', count)
+
+  socket.on('increment', () => {
+    count++
+    // socket.emit('countUpdated', count)
+    io.emit('countUpdated', count)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`server on the ${port}`)
 })
