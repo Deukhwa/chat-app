d777e3e22e9ac3ed9ad251e507dae559b2180f4b
Deukhwa <deukhwajeong@gmail.com>
2021-10-29 20:50:09 -0400

    Socket.io - Acknowledgement / Filter

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,33 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-  console.log('The count has been updated', count)
+socket.on('message', (msg) => {
+  console.log(msg)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('Clicked')
-  socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const message = document.querySelector('.textMsg').value
+
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log('Message delivered')
+  })
+})
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+  e.preventDefault()
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your brower')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords
+    socket.emit('sendLocation', { latitude, longitude }, () => {
+      console.log('Location shared')
+    })
+  })
 })
